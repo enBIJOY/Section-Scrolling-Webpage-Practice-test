c4a2862016a4ceb5085a6c24e7c4048b88c96347
enBIJOY <162016650+enBIJOY@users.noreply.github.com>
2026-01-14 15:57:35 +0600

    Section Scroll Webpage Update
    ->Section Scroll
    -> Layout Mastery
    -> Jetsream Auth
    -> Nav Edit
    -> Some CSS

---
+++ resources/views/website/layout/master.blade.php
@@ -0,0 +1,32 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Master Blade Title</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <style>
+        body{
+    scroll-behavior: smooth;
+    padding-top: 70px;
+    }
+    
+    </style>
+    
+  </head>
+  <body>
+    @include('website.layout.nav')
+
+        @yield('content')
+
+
+    <!--  Page Scroll to Top  -->
+
+    <a id="scroll-to-top" class="scroll-to-top js-scroll-trigger" href="#top-header">
+      <i class="fa fa-angle-up"></i>
+    </a>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    @include('website.layout.footer')
+  </body>
+</html>
---
+++ resources/views/website/layout/nav.blade.php
@@ -0,0 +1,28 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top">
+  <div class="container-fluid container">
+    <a class="navbar-brand" href="#home">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+      <ul class="navbar-nav mx-auto">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#home">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#about">about</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#features">features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#contact">contact</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="button">
+        <a class="btn btn-outline-success" type="submit" href="{{('login')}}"><i class="bi bi-person"></i>login</a>
+        <a class="btn btn-outline-success" type="submit" href="{{('register')}}"><i class="bi bi-person"></i>register</a>
+      </form>
+    </div>  
+  </div>
+</nav>

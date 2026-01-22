31c78a4df409be90eba1ffdc427e8aac45a4263d
enBIJOY <mofizuldev@gmail.com>
2026-01-22 12:02:45 +0600

    Newsletter->DB (Ajax) without Reaload

--- database/migrations/2026_01_14_082709_create_products_table.php
+++ database/migrations/2026_01_14_082709_create_products_table.php
@@ -13,10 +13,10 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->nullable;
-            $table->string('email')->nullable;
-            $table->string('phone')->nullable;
-            $table->string('description')->nullable;
+            $table->string('name')->nullable();
+            $table->string('email')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('description')->nullable();
             $table->timestamps();
         });
     }
